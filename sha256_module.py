e02f45416e8f091c618e809be0b60a904cdc7ac4
Tenkoni <caliminion@gmail.com>
2019-09-30 10:43:52 -0500

    SHA-2 (256-bits) using the proposed time-testing structure in initial_testing

--- sha256_module.py
+++ sha256_module.py
@@ -4,15 +4,15 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 
 
-digest = hashes.Hash(hashes.SHA256(), backend = default_backend())
-digest.update(b"a")
-a = digest.finalize()
-print(a.hex())
-# time_list = []
-# for elem in inp:
-# 	start = timer()
-# 	testo_funco(elem)
-# 	end = timer()
-# 	time_list.append(end-start)
-# 	print("This time:" + str(end - start))
-# print("Average time: " + str(sum(time_list)/len(time_list)))
+def sha256_timer(test_vectors): #the test_vector must have ONLY bytes-type elements
+    """Hash each element of test_vector and returns a list with execution times"""
+	time_list = [] #list to save each execution time
+	for vector in test_vectors:
+		start = timer()
+		digest = hashes.Hash(hashes.SHA256(), backend = default_backend()) #initialize hash digest
+		digest.update(vector) #hash vector
+		a = digest.finalize() #output hash
+		end = timer()
+		#print(str(vector)+" ->" + str(a.hex())) #aux to verify proper hashing
+		time_list.append(end-start)
+	return(time_list)

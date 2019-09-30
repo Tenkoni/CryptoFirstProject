5b972d830833f5159b5c4765c9b9779c3abf40dd
Tenkoni <caliminion@gmail.com>
2019-09-30 10:52:48 -0500

    SHA-1 using the proposed time-testing structure in initial_testing

---
+++ sha1_module.py
@@ -0,0 +1,19 @@
+#sha-1 module
+from timeit import default_timer as timer
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+
+
+def sha1_timer(test_vectors): #the test_vector must have ONLY bytes-type elements
+	"""Hash (sha-1) each element of test_vector and returns a list with execution times"""
+	time_list = [] #list to save each execution time
+	for vector in test_vectors:
+		start = timer()
+		digest = hashes.Hash(hashes.SHA1(), backend = default_backend()) #initialize hash digest
+		digest.update(vector) #hash vector
+		a = digest.finalize() #output hash
+		end = timer()
+		#print(str(vector)+" ->" + str(a.hex())) #aux to verify proper hashing
+		time_list.append(end-start)
+	return(time_list)
+

f0b7a6b4d6741799c260e9786827b615abba0b64
Tenkoni <caliminion@gmail.com>
2019-09-30 11:09:13 -0500

    MD5 using the proposed time-testing structure in initial_testing

---
+++ md5_module.py
@@ -0,0 +1,18 @@
+#md5 module
+from timeit import default_timer as timer
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+
+
+def md5_timer(test_vectors): #the test_vector must have ONLY bytes-type elements
+	"""Hash (md5) each element of test_vector and returns a list with execution times"""
+	time_list = [] #list to save each execution time
+	for vector in test_vectors:
+		start = timer()
+		digest = hashes.Hash(hashes.MD5(), backend = default_backend()) #initialize hash digest
+		digest.update(vector) #hash vector
+		a = digest.finalize() #output hash
+		end = timer()
+		#print(str(vector)+" ->" + str(a.hex())) #aux to verify proper hashing
+		time_list.append(end-start)
+	return(time_list)

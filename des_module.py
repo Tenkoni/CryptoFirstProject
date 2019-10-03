424206e938e3257f065bd39aa53ebf7ca02eac42
Tenkoni <caliminion@gmail.com>
2019-10-02 19:55:31 -0500

    Added timing structure and return values, still needs to adjust for a proper integration to the test platform

--- des_module.py
+++ des_module.py
@@ -2,6 +2,7 @@
 import secrets
 from des import DesKey
 from cryptography.hazmat.primitives import padding
+from timeit import default_timer as timer
 
 def des_timer(test_vectors):
 	"""Encrypts (DES) each element of test_vector with a padding (64 bits) 
@@ -15,20 +16,26 @@ def des_timer(test_vectors):
 		#creating padding instance
 		padder = padding.PKCS7(64).padder()
 		#encrypting begins (using CBC mode)
+		start_e = timer()
 		padded_m = padder.update(vector) + padder.finalize() #padding is included in the process as instructed
 		cyphertext = des_instance.encrypt(padded_m, initial = iv)
+		end_e = timer()
 		#encrypting ends, timing this section
 		print(str(cyphertext))
 		#creating unpadding instance
 		unpadder = padding.PKCS7(64).unpadder()
 		#decrypting begins 
+		start_d = timer()
 		plaintext_d = des_instance.decrypt(cyphertext, initial = iv)
 		plaintext = unpadder.update(plaintext_d) + unpadder.finalize()
+		end_d = timer()
 		#decryption ends
 		print(plaintext)
+		timelist.append((end_e - start_e, end_d - start_d))
+	return timelist
 
-benin = [b"heloelelello", b"mahler is also a great composer", b"tuturuuuuuuuuuuuuuuuuuuuuuuus"]
-des_timer(benin)
+benin = [b"heloelelello", b"mahler is also a great composer", b"tuturuuuuufdfsddddddddduuuuuuuuuuuuuuuuuus"]
+print(des_timer(benin))
 
 
 

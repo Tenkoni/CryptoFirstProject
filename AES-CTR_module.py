d5d1ae184bcdd7f517a7ef425cd958a6b3b7301c
Tenkoni <caliminion@gmail.com>
2019-10-03 01:27:52 -0500

    AES-CTR (Counter mode) ready to add to the testing platform

--- AES-CTR_module.py
+++ AES-CTR_module.py
@@ -1,5 +1,4 @@
 #AES-CTR
-import sys
 import secrets
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
@@ -24,19 +23,15 @@ def aes_ofb_timer(test_vectors):
 		start_e = timer()
 		ciphertext = encryptor.update(vector) + encryptor.finalize()
 		end_e = timer()
-		print(len(ciphertext.hex()))
+		#print(len(ciphertext.hex()))
 		#encryption ends -this is the fragment we should time for encryption time
-		print("ciphertext: " + str(ciphertext.hex()))
+		#print("ciphertext: " + str(ciphertext.hex()))
 		decryptor = cipher.decryptor()
 		#decryption begins
 		start_d = timer()
 		plaintext_d = decryptor.update(ciphertext) + decryptor.finalize()
 		end_d = timer()
 		#decryption ends -this is the fragment we should time for decryption time
-		print("Deciphered text: "+ str(plaintext_d))
+		#print("Deciphered text: "+ str(plaintext_d))
 		timelist.append((end_e - start_e, end_d - start_d))
 	return timelist
-
-
-benin = [b"stre", b"thisisateststreamjusttoseeifthisworks", b"yayitworks"]
-print(aes_ofb_timer(benin))
